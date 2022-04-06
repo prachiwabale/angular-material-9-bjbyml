d8893da3be1860e92596e3daddd92bd537199093
prachiwabale <prachiwabale2091993@gmail.com>
2022-04-06 18:04:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-9-bjbyml
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-9-bjbyml)
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import 'hammerjs';
+import { AppComponent } from './app.component';
+
+import { MaterialModule } from './material.module';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpErrorHandler } from './http-error-handler.service';
+import { TableComponent } from './table/table.component';
+import { BadgeComponent } from './badge/badge.component';
+
+const appRoutes: Routes = [{ path: '', component: AppComponent }];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, {
+      enableTracing: true,
+    }),
+  ],
+  declarations: [AppComponent, TableComponent,BadgeComponent],
+  bootstrap: [AppComponent],
+  exports: [RouterModule],
+  providers: [HttpErrorHandler],
+})
+export class AppModule {}
