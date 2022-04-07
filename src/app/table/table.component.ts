3cbb1ecb056579fc6268c6da546954faeeef7d34
prachiwabale <prachiwabale2091993@gmail.com>
2022-04-07 12:13:24 +0530

    4 files modified

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,28 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
-  name: string;
   position: number;
+  name: string;
   dateofbirth: string;
   effectivedate: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', dateofbirth: '', effectivedate: 'H'},
-  {position: 2, name: 'Helium', dateofbirth: '', effectivedate: 'He'},
- 
+  {
+    position: 1,
+    name: 'Abc',
+    dateofbirth: '07-04-2022',
+    effectivedate: '07-04-2022',
+  },
+  {
+    position: 2,
+    name: 'Pqq',
+    dateofbirth: '15-04-2022',
+    effectivedate: '15-04-2022',
+  },
 ];
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'dateofbirth', 'effectivedate'];
+  displayedColumns: string[] = [
+    'position',
+    'name',
+    'dateofbirth',
+    'effectivedate',
+  ];
   dataSource = ELEMENT_DATA;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
